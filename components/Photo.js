390ce57a46f33e783d6a16dee2f09dda693bedeb
Lucas_J <82843508+Otwgoat@users.noreply.github.com>
2023-01-11 09:33:32 +0100

    14th test

--- components/Photo.js
+++ components/Photo.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Image from 'next/image'
+
 
 const Photo = ({photo}) => {
   return (
     <div className='photoContainer'>
-        <img src={photo.image} width={400} height={500} alt={photo.alt}></img>
+        <img className='photoSquare' src={photo.image} width={400} height={500} alt={photo.alt}></img>
     </div>
   )
 }
