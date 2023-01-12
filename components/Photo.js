3a1964d830fe04b79f76d983f7e49d1aef837e07
Lucas_J <82843508+Otwgoat@users.noreply.github.com>
2023-01-12 09:47:58 +0100

    16th commit

--- components/Photo.js
+++ components/Photo.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Photo = ({photo}) => {
   return (
     <div className='photoContainer'>
-        <Image className='photoSquare' src={'/' + photo.image} width={photo.width} height={photo.height} alt={photo.alt} />
+        <Image className='photoSquare' src={'/' + photo.image} width={400} height={500} alt={photo.alt} />
     </div>
   )
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Meta from '../components/Meta'
 
 
@@ -15,9 +15,26 @@ const portfolio = (props) => {
     const [familyIsActive, setFamilyIsActive] = useState(false);
     const [baptismIsActive, setBaptismIsActive] = useState(false);
     const [coupleIsActive, setCoupleIsActive] = useState(false);
+    const [portraitIsActive, setPortraitIsActive] = useState(false);
+
+    useEffect(() => {
+        if (weddingIsActive || babyIsActive || pregnancyIsActive || familyIsActive || baptismIsActive || coupleIsActive || portraitIsActive) {
+            setAllIsActive(false)
+        } else {
+            setAllIsActive(true)
+        }
+        
+    })
 
     const handleToggleAllItem = () => {
-        setAllIsActive(!allIsActive)
+        setWeddingIsActive(false)
+        setBabyIsActive(false)
+        setPregnancyIsActive(false)
+        setFamilyIsActive(false)
+        setBaptismIsActive(false)
+        setCoupleIsActive(false)
+        setPortraitIsActive(false)
+        setAllIsActive(true)
     }
     const handleToggleWeddingItem = () => {
         setWeddingIsActive(!weddingIsActive)
@@ -37,6 +54,10 @@ const portfolio = (props) => {
     const handleToggleCoupleItem = () => {
         setCoupleIsActive(!coupleIsActive)
     }
+    const handleTogglePortraitItem = () => {
+        setPortraitIsActive(!portraitIsActive)
+    }
+    
     
 
   return (
@@ -47,21 +68,30 @@ const portfolio = (props) => {
             <p className='descriptionContainer'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in sapien eros. Phasellus porta diam sit amet ex aliquam rhoncus. Nulla consectetur risus et condimentum pellentesque. Ut rhoncus purus dignissim condimentum commodo. Vivamus consectetur urna ut arcu mollis suscipit. Integer tellus ipsum, tempor mattis blandit vitae, ullamcorper et tortor. Curabitur vestibulum nunc nec lacus condimentum, ut bibendum sapien aliquet. Vestibulum vitae diam congue, consequat lectus at, dictum nisl. Donec tempus ante eleifend vestibulum consequat. Donec semper nisi non mi blandit, sed suscipit sapien faucibus. Vestibulum quis efficitur est. Vestibulum ex sem, commodo dictum hendrerit vel, eleifend eu sem. Morbi vehicula.</p>
             <ul id='filterNav'>
                 
-                <li key='all' id='all' className={allIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleAllItem}>Tout</li>
-                <li key='wedding' id='wedding' className={weddingIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleWeddingItem}>Mariage</li>
+                <li key='all' id='first' className={allIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleAllItem}>Tout</li>
+                <li key='wedding' className={weddingIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleWeddingItem}>Mariage</li>
                 <li key='pregnancy' className={pregnancyIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleTogglePregnancyItem}>Grossesse</li>
                 <li key='baby' className={babyIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleBabyItem}>Bébé</li>
                 <li key='family' className={familyIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleFamilyItem}>Famille</li>
                 <li key='baptism' className={baptismIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleBaptismItem}>Baptême</li>
                 <li key='couple' className={coupleIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleToggleCoupleItem}>Couple</li>
+                <li key='portrait' id='last' className={portraitIsActive ? 'filterItemActive' : 'filterItem'} onClick={handleTogglePortraitItem}>Portrait</li>
                 
             </ul>
             
             <div className='photosContainer'>
                 {photos.map((photo) => (
-                    <Photo photo={photo} key={photo.title} />
-                    
+                    allIsActive &&
+                    <Photo photo={photo} key={photo.title} />  
                 ))}
+                
+                   
+                
+                  
+                  
+                    
+                
+                
 
             </div>
         </div>
