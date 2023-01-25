5d60fa4ed29ceb2fc14215f48928dab5cd8deb8e
Lucas_J <82843508+Otwgoat@users.noreply.github.com>
2023-01-25 11:23:54 +0100

    commit 23

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Meta from '../components/Meta'
 import Description from '../components/Description'
+import fsPromises from 'fs/promises';
+import path from 'path';
+import Input from '../components/Input';
 
-const contact = () => {
+const contact = (props) => {
+    let inputs = props.objectData;
   return (
     <div className='page container'>
         <Meta title={'Charles Cantin - Me contacter'} description={'Une envie , une idée, un projet ? Contactez moi via le formulaire sur cette page, je m\'engage à y repondre dans les 24h.' } />
@@ -10,7 +14,10 @@ const contact = () => {
             <h2>Me contacter</h2>
             <Description />
             <form name='contact' method='POST' data-netlify="true">
-                <input className='inputText' type='text' name='lastname' placeholder='Nom' />
+                {inputs.map((input) => (
+                    <Input name={input.name} type={input.type} placeholder={input.placeholder} />
+                ))}
+                
             </form>
             
 
@@ -21,3 +28,20 @@ const contact = () => {
 }
 
 export default contact
+/*** Import the data of the portfolio's json *********/
+export const getStaticProps = async () => {
+    
+    const filePath = path.join(process.cwd(), 'content/contact.json')
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData)
+
+    
+    return {
+        props: objectData,
+            
+    }
+
+    
+    
+    
+}
