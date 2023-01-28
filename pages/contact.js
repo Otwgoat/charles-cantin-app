558fd7ddeeb2fa6a7fc178b39977cbf19b62f892
Lucas_J <82843508+Otwgoat@users.noreply.github.com>
2023-01-28 11:54:16 +0100

    commit 23

--- pages/contact.js
+++ pages/contact.js
@@ -9,27 +9,7 @@ import Input from '../components/Input';
 const contact = (props) => {
     let inputs = props.inputs;
     
-    function encode(data) {
-        return Object.keys(data)
-          .map(
-            (key) =>
-              encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-          )
-          .join("&");
-      }
-      
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({
-            "form-name": event.target.getAttribute("name")
-          }),
-        })
-          .then(() => alert('Nous avons bien reçu votre message'))
-          .catch((error) => alert(error));
-      };
+    
   
     return (
     <div className='page container'>
@@ -37,7 +17,7 @@ const contact = (props) => {
         <div className='contactContainer'>
             <h2>Me contacter</h2>
             <Description />
-            <form name='contact' method='POST' netlify netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+            <form name='contact' method='POST' action='/pages/success.js' netlify netlify-honeypot="bot-field" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact" />
                 {inputs.map((input) => (
                     <Input key={input.name} id={input.name} input={input} />
---
+++ pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const success = () => {
+  return (
+    <div className='page container'>
+        <h3>Merci pour votre soumission</h3>
+    </div>
+  )
+}
+
+export default success
